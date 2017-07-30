4eefedeb1f968f94e20aa3738e320cc1f938baf2
Eoghan O'Loughlin <eoghanoloughlin@ymail.com>
2017-07-31 00:41:37 +0100

    Improved currency field tests

--- test/model.js
+++ test/model.js
@@ -2,9 +2,11 @@ const chai = require('chai');
 const expect = chai.expect;
 const mongoose = require('mongoose');
 const Promise = require('bluebird');
+const currencies = require('country-data').currencies;
 const Message = require('../model/message');
 const Errors = require('../config/errors').get('model').Message;
 
+// A valid message object
 const data = {
     userId: 134256,
     currencyFrom: 'EUR',
@@ -16,8 +18,7 @@ const data = {
     originatingCountry: 'FR'
 };
 
-describe('Model', () => {
-
+describe('Message', () => {
     /**
      * Set up DB connection
      */
@@ -35,9 +36,12 @@ describe('Model', () => {
 
     describe('userId', () => {
         it('should be a number', (done) => {
-            const ids = ['fifty six', null];
+            /**
+             * First, verify bad data will fail validation
+             */
+            const testIds = ['fifty six', null];
 
-            Promise.all(ids.map((userId) => {
+            Promise.all(testIds.map((userId) => {
                 const msg = Object.assign({}, data, { userId });
 
                 return new Message(msg).save().reflect();
@@ -47,38 +51,103 @@ describe('Model', () => {
                 expect(result).to.be.an('object');
                 expect(result).to.have.property('errors');
 
-            }).then(done());
+            }).then(() => {
+                /**
+                 * Then, verify good data will pass validation
+                 */
+                const msg = Object.assign({}, data, {
+                    userId: 457
+                });
+
+                new Message(msg).save((err, message) => {
+                    expect(err).to.equal(null);
+                    expect(message).to.be.an('object');
+                    expect(message).to.have.property('_id');
+
+                    done();
+                });
+            });
         });
     });
 
     describe('currencyFrom', () => {
         it('should be a valid currency code', (done) => {
-            const msg = Object.assign({}, data, {
-                currencyFrom: 'some string that is not a currency code',
-            });
+            /**
+             * First, verify bad data will fail validation
+             */
+            const testCurrencies = [45, false, 'some string that is not a currency code'];
 
-            new Message(msg).save((err, message) => {
-                expect(err).to.be.an('object');
-                expect(err.errors).to.have.property('currencyFrom');
-                expect(err.errors.currencyFrom.message).to.equal(Errors.currencyFrom);
+            Promise.all(testCurrencies.map((currency) => {
+                const msg = Object.assign({}, data, {
+                    currencyFrom: currency
+                });
 
-                done();
+                return new Message(msg).save().reflect();
+            })).each((inspection) => {
+                const result = inspection.reason();
+
+                expect(result).to.be.an('object');
+                expect(result.errors).to.have.property('currencyFrom');
+                expect(result.errors.currencyFrom.message).to.equal(Errors.currencyFrom);
+
+            }).then(() => {
+                /**
+                 * Then, verify good data will pass validation
+                 */
+                const validCurrencyCode = currencies[Object.keys(currencies)[10]].code; // Random valid currency code
+
+                const msg = Object.assign({}, data, {
+                    currencyFrom: validCurrencyCode
+                });
+
+                new Message(msg).save((err, message) => {
+                    expect(err).to.equal(null);
+                    expect(message).to.be.an('object');
+                    expect(message).to.have.property('_id');
+
+                    done();
+                });
             });
         });
     });
 
     describe('currencyTo', () => {
         it('should be a valid currency code', (done) => {
-            const msg = Object.assign({}, data, {
-                currencyTo: 'somestringthatisnotacurrencycode',
-            });
+            /**
+             * First, verify bad data will fail validation
+             */
+            const testCurrencies = [45, false, 'somestringthatisnotacurrencycode'];
 
-            new Message(msg).save((err, message) => {
-                expect(err).to.be.an('object');
-                expect(err.errors).to.have.property('currencyTo');
-                expect(err.errors.currencyTo.message).to.equal(Errors.currencyTo);
+            Promise.all(testCurrencies.map((currency) => {
+                const msg = Object.assign({}, data, {
+                    currencyTo: currency
+                });
 
-                done();
+                return new Message(msg).save().reflect();
+            })).each((inspection) => {
+                const result = inspection.reason();
+
+                expect(result).to.be.an('object');
+                expect(result.errors).to.have.property('currencyTo');
+                expect(result.errors.currencyTo.message).to.equal(Errors.currencyTo);
+
+            }).then(() => {
+                /**
+                 * Then, verify good data will pass validation
+                 */
+                const validCurrencyCode = currencies[Object.keys(currencies)[20]].code; // Random valid currency code
+
+                const msg = Object.assign({}, data, {
+                    currencyTo: validCurrencyCode
+                });
+
+                new Message(msg).save((err, message) => {
+                    expect(err).to.equal(null);
+                    expect(message).to.be.an('object');
+                    expect(message).to.have.property('_id');
+
+                    done();
+                });
             });
         });
     });
@@ -116,5 +185,4 @@ describe('Model', () => {
 
         done();
     });
-
 });
