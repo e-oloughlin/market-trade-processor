0b1325b0b0427375999c06e47029473eed19267b
Eoghan O'Loughlin <eoghanoloughlin@ymail.com>
2017-08-19 01:41:05 +0100

    Replaced requirejs with browserify

--- src/js/app/view/table-view.js
+++ src/js/app/view/table-view.js
@@ -1,123 +1,112 @@
-/**
- * A backbone view for the main
- * table of messages
- */
-define('app/view/table-view', [
-    'backbone',
-    'handlebars',
-    'util/pubsub',
-    'text!templates/table.hbs',
-    'text!templates/table-row.hbs',
-    'bsTab'
-], function(Backbone, handlebars, pubsub, tableTemplate, tableRowTemplate) {
+var $           = require('jquery'),
+    _           = require('underscore'),
+    Backbone    = require('backbone'),
+    handlebars  = require('hbsfy/runtime'),
+    bootstrap   = require('bootstrap'),
+    pubsub      = require('../../util/pubsub'),
+    table       = require('../../templates/table.hbs'),
+    tableRow    = require('../../templates/table-row.hbs');
 
-    // Register a table row partial for use within
-    // a parent table template
-    handlebars.registerPartial('table-row', tableRowTemplate);
+// Register a table row partial for use within
+// a parent table template
+handlebars.registerPartial('table-row', tableRow);
 
-    // Compile the handlebars templates needed
-    // for this view
-    var table = handlebars.compile(tableTemplate),
-        tableRow = handlebars.compile(tableRowTemplate);
+module.exports = Backbone.View.extend({
+    el: '#message-list',
 
-    return Backbone.View.extend({
-        el: '#message-list',
+    initialize: function(options) {
+        // Ensure the context of renderTableRow is always this view
+        _.bindAll(this, 'renderTableRow', 'highlightRow');
 
-        initialize: function(options) {
-            // Ensure the context of renderTableRow is always this view
-            _.bindAll(this, 'renderTableRow', 'highlightRow');
-
-            if(options.collection) {
-                this.render();
-            }
-        },
-
-        /**
-         * Render a table of all messages
-         * @return {Object}    This view
-         */
-        render: function() {
-            this.$el.html(table(this.collection.toJSON()));
-
-            pubsub.subscribe('go-to-row', this.highlightRow);
-
-            return this;
-        },
-
-        /**
-         * Show this table tab
-         * @return {Object}    This view
-         */
-        showTable: function() {
-            $('a[href="#'+this.el.id+'"]').tab('show');
-
-            return this;
-        },
-
-        /**
-         * Render a table row for an individual message
-         * @param  {Backbone.Model}
-         */
-        renderTableRow: function(model) {
-            var data = _.extend({
-                newRow: true
-            }, model.toJSON()),
-
-            $row = $(tableRow(data));
-
-            this.$el.find('tbody').append($row);
-
-            this.scrollToRow(model.get('_id'));
-
-            this.unHighlightRow($row);
-        },
-
-        /**
-         * Return a row by its id attribute
-         * @param  {Number}
-         * @return {jQuery}
-         */
-        getRow: function(id) {
-            return this.$el.find('[data-id="'+id+'"]');
-        },
-
-        /**
-         * Highlight a row
-         * @param  {Number} id      id attribute of row
-         */
-        highlightRow: function(id) {
-            this.showTable();
-
-            var $row = this.getRow(id);
-
-            this.scrollToRow(id);
-
-            $row.addClass('green-flash');
-
-            this.unHighlightRow($row);
-        },
-
-        /**
-         * Scroll to a particular row
-         * @param  {Number} id      id attribute of row
-         */
-        scrollToRow: function(id) {
-            this.$el.stop().animate({
-                scrollTop: this.getRow(id).position().top
-            }, 750);
-        },
-
-        /**
-         * Prevent the row being reanimated
-         * when it goes out and into view again,
-         * eg. when tabbing
-         * @param  {jQuery}     $row
-         */
-        unHighlightRow: function($row) {
-            setTimeout(function() {
-                $row.removeClass('green-flash');
-            }, 2000)
+        if(options.collection) {
+            this.render();
         }
-    });
+    },
 
+    /**
+     * Render a table of all messages
+     * @return {Object}    This view
+     */
+    render: function() {
+        this.$el.html(table(this.collection.toJSON()));
+
+        pubsub.subscribe('go-to-row', this.highlightRow);
+
+        return this;
+    },
+
+    /**
+     * Show this table tab
+     * @return {Object}    This view
+     */
+    showTable: function() {
+        $('a[href="#'+this.el.id+'"]').tab('show');
+
+        return this;
+    },
+
+    /**
+     * Render a table row for an individual message
+     * @param  {Backbone.Model}
+     */
+    renderTableRow: function(model) {
+        var data = _.extend({
+            newRow: true
+        }, model.toJSON()),
+
+        $row = $(tableRow(data));
+
+        this.$el.find('tbody').append($row);
+
+        this.scrollToRow(model.get('_id'));
+
+        this.unHighlightRow($row);
+    },
+
+    /**
+     * Return a row by its id attribute
+     * @param  {Number}
+     * @return {jQuery}
+     */
+    getRow: function(id) {
+        return this.$el.find('[data-id="'+id+'"]');
+    },
+
+    /**
+     * Highlight a row
+     * @param  {Number} id      id attribute of row
+     */
+    highlightRow: function(id) {
+        this.showTable();
+
+        var $row = this.getRow(id);
+
+        this.scrollToRow(id);
+
+        $row.addClass('green-flash');
+
+        this.unHighlightRow($row);
+    },
+
+    /**
+     * Scroll to a particular row
+     * @param  {Number} id      id attribute of row
+     */
+    scrollToRow: function(id) {
+        this.$el.stop().animate({
+            scrollTop: this.getRow(id).position().top
+        }, 750);
+    },
+
+    /**
+     * Prevent the row being reanimated
+     * when it goes out and into view again,
+     * eg. when tabbing
+     * @param  {jQuery}     $row
+     */
+    unHighlightRow: function($row) {
+        setTimeout(function() {
+            $row.removeClass('green-flash');
+        }, 2000)
+    }
 });
--- src/js/main.js
+++ src/js/main.js
@@ -1,63 +1,42 @@
-require.config({
-    paths: {
-        domReady: 'lib/domReady/domReady',
-        jquery: 'lib/jquery/dist/jquery',
-        backbone: 'lib/backbone/backbone',
-        underscore: 'lib/underscore/underscore',
-        handlebars: 'lib/handlebars/handlebars',
-        lodash: 'lib/lodash/lodash',
-        bsTab: 'lib/bootstrap/js/tab',
-        text: 'lib/text/text',
-        socketio: '/socket.io/socket.io.js',
-        async: 'lib/requirejs-plugins/src/async',
-    },
-    shim: {
-        socketio: {
-            exports: 'io'
-        },
-        bsTab: {
-            deps: ['jquery']
+// Dependencies
+var $ = require('jquery');
+
+global.jQuery = $;
+
+var bootstrap = require('bootstrap'),
+    messageCollection = require('./app/collection/message-collection'),
+    tableView = require('./app/view/table-view'),
+    mapView = require('./app/view/map-view');
+
+var app = {
+    messages: new messageCollection()
+};
+
+// DOM Ready
+$(function() {
+    var $nav = $('.navbar'),
+        navMargin = parseFloat($nav.css('margin-bottom'));
+
+    // Set each tab pane to have a max height of the viewport
+    $('.tab-pane').css('height', $(window).height() - $nav.outerHeight() - (navMargin * 2));
+
+    // Grab initial messages from the server
+    app.messages.fetch({
+        data: {
+            depth: 1 // Request child objects
         }
-    }
-});
-
-require([
-    'domReady',
-    'jquery',
-    'app/collection/message-collection',
-    'app/view/table-view',
-    'app/view/map-view',
-    'bsTab'
-], function(domReady, $, messageCollection, tableView, mapView) {
-    var app = {
-        messages: new messageCollection()
-    };
-
-    domReady(function() {
-        var $nav = $('.navbar'),
-            navMargin = parseFloat($nav.css('margin-bottom'));
-
-        // Set each tab pane to have a max height of the viewport
-        $('.tab-pane').css('height', $(window).height() - $nav.outerHeight() - (navMargin * 2));
-
-        // Grab initial messages from the server
-        app.messages.fetch({
-            data: {
-                depth: 1 // Request child objects
-            }
-        }).done(function() {
-            // Initialize the map view
-            app.mapView = new mapView({
-                collection: app.messages
-            });
-
-            // Initialize a new table view for all messages
-            app.tableView = new tableView({
-                collection: app.messages
-            });
-
-            // Listen for any further live additions (socket.io)
-            app.messages.on('add', app.tableView.renderTableRow);
+    }).done(function() {
+        // Initialize the map view
+        app.mapView = new mapView({
+            collection: app.messages
         });
+
+        // Initialize a new table view for all messages
+        app.tableView = new tableView({
+            collection: app.messages
+        });
+
+        // Listen for any further live additions (socket.io)
+        app.messages.on('add', app.tableView.renderTableRow);
     });
 });
